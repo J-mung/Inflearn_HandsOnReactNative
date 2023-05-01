76b3c39bb6f1499d64151999cb7ac23b42975cee
TAEGYU JANG <xorb2298@gmail.com>
2023-05-01 14:24:15 +0900

    Feat empty list screen

---
+++ src/components/List.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { GRAY } from '../colors';
+import ListItem from './ListItem';
+
+const Separator = () => {
+  return <View style={styles.separator}></View>;
+};
+
+const List = ({ data }) => {
+  return (
+    <FlatList
+      data={data}
+      renderItem={({ item }) => <ListItem item={item} />}
+      windowSize={5} // prev : 2, cur: 1, next: 2
+      ItemSeparatorComponent={Separator}
+      ListHeaderComponent={View}
+      ListHeaderComponentStyle={{ height: 10 }}
+    />
+  );
+};
+
+List.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 1,
+    backgroundColor: GRAY.LIGHT,
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+});
+
+export default List;
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,38 +1,10 @@
-import { FlatList, StyleSheet, View } from 'react-native';
-import { GRAY } from '../colors';
-import ListItem from '../components/ListItem';
-
-const Separator = () => {
-  return <View style={styles.separator}></View>;
-};
-const styles = StyleSheet.create({
-  separator: {
-    height: 1,
-    backgroundColor: GRAY.LIGHT,
-    marginVertical: 10,
-    marginHorizontal: 10,
-  },
-});
+import EmptyList from '../components/EmptyList';
+import List from '../components/List';
 
 const ListScreen = () => {
-  const todos = [
-    { id: 1, task: 'React Native 1', isDone: false },
-    { id: 2, task: 'React Native 2', isDone: true },
-    { id: 3, task: 'React Native 3', isDone: false },
-    { id: 4, task: 'React Native 4', isDone: true },
-    { id: 5, task: 'React Native 5', isDone: false },
-  ];
+  const todos = [{ id: 1, task: 'tete', isDone: false }];
 
-  return (
-    <FlatList
-      data={todos}
-      renderItem={({ item }) => <ListItem item={item} />}
-      windowSize={5} // prev : 2, cur: 1, next: 2
-      ItemSeparatorComponent={Separator}
-      ListHeaderComponent={View}
-      ListHeaderComponentStyle={{ height: 10 }}
-    />
-  );
+  return todos.length ? <List data={todos} /> : <EmptyList />;
 };
 
 export default ListScreen;
